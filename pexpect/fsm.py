a38818805818787aaa151701a580c0e8c2c6c77a
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2002-08-15 05:08:10 +0000

    Newish FSM.

--- pexpect/fsm.py
+++ pexpect/fsm.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 '''This module implements a Finite State Machine (FSM) with one stack.
-The FSM is fairly simple. It is useful for small parsing tasks.
 The addition of a stack makes it much simpler to build tiny parsers.
+The FSM is fairly simple. It is useful for small parsing tasks.
 
-The FSM is an association of
+This FSM is defined by an association of
     (input_symbol, current_state) --> (action, next_state)
+You can also define a association of 
+                  (current_state) --> (action, next_state)
+Which is check if no (input_symbol, current_state) matches.
+Finally, if none of these match then the FSM checks to see
+if the default transition has been set. If this has been
+set that that action is called and state is set.
+
 When the FSM matches the pair (input_symbol, current_state)
-it will call the associated action and then set the next state.
-The action will be passed input_symbol, current state, and a stack.
+it will call the associated function "action" and then set the next state.
+The action will be passed input_symbol, and the FSM.
+You can use None for an action in which case this class
+behaves just like a simple Finite State Machine where only the
+next state is set.
+
+Noah Spurrier 20020814
 '''
 
-class ANY:
-    '''This is a meta key. This is a class, but you use it like a value.
-    Example: x = ANY
-    Example: f.add_transaction (ANY, 'SOMESTATE', None, 'OTHERSTATE')
-    '''
-    pass
+class ExceptionFSM(Exception):
+    '''Base class for all exceptions raised by this module.'''
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return `self.value`
 
 class FSM:
     '''This class is a Finite State Machine (FSM) with one stack.
@@ -34,6 +46,7 @@ class FSM:
 
     def __init__(self, initial_state = None):
         self.state_transitions = {}   # Map (input_symbol, state) to (action, next_state).
+        self.state_transitions_any = {} # Map (state) to (action, next_state).
         self.default_transition = None
         self.initial_state = initial_state
         self.current_state = self.initial_state
@@ -52,16 +65,19 @@ class FSM:
         self.current_state = self.initial_state
         self.stack = []
 
-    def add_default_transition (self, action, next_state):
-        '''This sets the default transition.
-        If the FSM cannot match the pair (input_symbol, current_state)
-        in the transition table then this is the transition that 
-        will be returned. This is useful for catching errors and undefined states.
+    def set_default_transition (self, action, next_state):
+        '''This sets the default transition. This is the action and state
+        that is set if the FSM cannot find the input_symbol and the
+        current_state.
+
+        This is useful for catching errors and undefined states.
         The default transition can be removed by calling
-        add_default_transition (None, None)
+        set_default_transition (None, None)
         If the default is not set and the FSM cannot match
         the input_symbol and current_state then it will 
-        raise an exception (see process()).
+        raise an exception (see process()).        
+
+        See also add_transition_any()
         '''
         if action == None and next_state == None:
             self.default_transition = None
@@ -69,21 +85,32 @@ class FSM:
             self.default_transition = (action, next_state)
 
     def add_transition (self, input_symbol, state, action, next_state):
-        '''This adds an association between inputs and outputs.
+        '''This adds an association between 
                 (input_symbol, current_state) --> (action, next_state)
            The action may be set to None.
-           The input_symbol may be set to None.
+           
+           You can also set transitions for a set of symbols by using
+           add_transition_list().
         '''
         self.state_transitions[(input_symbol, state)] = (action, next_state)
 
     def add_transition_list (self, list_input_symbols, state, action, next_state):
         '''This adds lots of the same transitions for different input symbols.
         You can pass a list or a string. Don't forget that it is handy to use
-        string.digits, string.letters, etc. to add transitions that match 
-        those character classes.
+        string.digits, string.whitespace, string.letters, etc. to add transitions 
+        that match character classes.
         '''
         for input_symbol in list_input_symbols:
             self.add_transition (input_symbol, state, action, next_state)
+
+    def add_transition_any (self, state, action, next_state):
+        '''This adds an association between any input_symbol
+        The process() method check these transitions after the
+        transitions that match a specific input symbol. The effect
+        is that these transitions match any input_symbol and a
+        specific current state.
+        '''
+        self.state_transitions_any [state] = (action, next_state)
 
     def get_transition (self, input_symbol, state):
         '''This tells what the next state and action would be 
@@ -91,21 +118,33 @@ class FSM:
         This returns (action, new state).
         This does not update the current state
         nor does it trigger the output action.
-        If the transition is not defined and the default state is defined
-        then that will be used; otherwise, this throws an exception.
+        
+        There are three steps in the sequence of checking for a defined transition.
+        This goes in order from the most specific to the least specific.
+        
+        1. First check state_transitions[] that match (input_symbol, current_state)
+        2. Second check state_transitions_any that match (current_state)
+           The effect is that any input_symbol that didn't match as part of
+           a state_transition[] will match if the state is defined here.
+        3. Third check if the default_transition is defined.
+           This matches any input_symbol and any state.
+           Theis is the handler for any error, undefined state, or defaults
+           that you want. Everything goes here if on handled by 1. or 2.
+        4. If the no transition is defined then raise an exception.
         '''
         if self.state_transitions.has_key((input_symbol, self.current_state)):
             return self.state_transitions[(input_symbol, self.current_state)]
-        elif self.state_transitions.has_key ((ANY, self.current_state)):
-            return self.state_transitions[(ANY, self.current_state)]
+        elif self.state_transitions_any.has_key (self.current_state):
+            return self.state_transitions_any[self.current_state]
         elif self.default_transition != None:
             return self.default_transition
         else:
-            raise Exception ('Transition is undefined.')
+            raise ExceptionFSM ('Transition is undefined.')
         
     def process (self, input_symbol):
-        '''This causes the fsm to change state and call an action.
-        (input_symbol, current_state) --> (action, next_state)
+        '''This is the main method you call to process input.
+        This causes the FSM to change state and call an action.
+            (input_symbol, current_state) --> (action, next_state)
         If the action is None then the action is not called and
         only the current state is changed.
         '''
@@ -115,9 +154,12 @@ class FSM:
         self.current_state = next_state
 
     def process_string (self, s):
+    	'''This takes each character of the string and sends it to process().
+    	'''
         for c in s:
             self.process (c)
 
+        
 ###################################################################
 # The following is a test of the FSM
 #
@@ -126,6 +168,8 @@ class FSM:
 # But it does check the tree structure and
 # can tell if the XML input is generally well formed or not.
 ####################################################################
+#http://www.w3.org/TR/REC-xml#NT-Name
+import string
 XML_TEST_DATA = '''<?xml version="1.0"?>
 <graph>
   <att name="directed" value="1" />
@@ -156,73 +200,87 @@ XML_TEST_DATA = '''<?xml version="1.0"?>
     <edge source="2" target="4" label="ab" />
 </graph>
 '''
+import sys
+
+def emit (s):
+    sys.stdout.write (s)
 def Error (input_symbol, fsm):
-    print 'UNDEFINED: %s, %s -- RESETTING' % (input_symbol, fsm.state)
+    print 'UNDEFINED: %s, %s -- RESETTING' % (input_symbol, fsm.current_state)
     fsm.reset()
+def Passthrough (input_symbol, fsm):
+    emit (input_symbol)
 def StartBuildTag (input_symbol, fsm):
     fsm.push (input_symbol)
+    emit (input_symbol)
 def BuildTag (input_symbol, fsm):
     s = fsm.pop ()
     s = s + input_symbol
     fsm.push (s)
+    emit (input_symbol)
 def DoneBuildTag (input_symbol, fsm):
     pass
+    emit (input_symbol)
 def DoneEmptyElement (input_symbol, fsm):
     s = fsm.pop()
-    print s
+    emit (input_symbol)
+    
 def StartBuildEndTag (input_symbol, fsm):
     fsm.push (input_symbol)
+    emit (input_symbol)
 def BuildEndTag (input_symbol, fsm):
     s = fsm.pop ()
     s = s + input_symbol
     fsm.push (s)
+    emit (input_symbol)
 def DoneBuildEndTag (input_symbol, fsm):
     s1 = fsm.pop ()
     s2 = fsm.pop ()
-    if s1 == s2:
-        print s1
-    else:
+    if s1 != s2:
         print 'Not valid XML.'
+    emit (input_symbol)
 
-def test():
+def demo ():
     f = FSM('INIT')
-    f.add_default_transition (Error, 'INIT')
-    f.add_transition ('<', 'INIT', None, 'TAG')
-    f.add_transition (ANY, 'INIT', None, 'INIT') # Ignore white space between tags
+    
+    f.set_default_transition (Error, 'INIT')
+    
+    f.add_transition ('<', 'INIT', Passthrough, 'TAG')
+    f.add_transition_any ('INIT', Passthrough, 'INIT')
 
-    f.add_transition ('?', 'TAG', None, 'XML_DECLARATION')
-    f.add_transition (ANY, 'XML_DECLARATION', None, 'XML_DECLARATION')
-    f.add_transition ('?', 'XML_DECLARATION', None, 'XML_DECLARATION_END')
-    f.add_transition ('>', 'XML_DECLARATION_END', None, 'INIT')
+    f.add_transition ('?', 'TAG', Passthrough, 'XML_DECLARATION')
+    f.add_transition_any ('XML_DECLARATION', Passthrough, 'XML_DECLARATION')
+    f.add_transition ('?', 'XML_DECLARATION', Passthrough, 'XML_DECLARATION_END')
+    f.add_transition ('>', 'XML_DECLARATION_END', Passthrough, 'INIT')
 
     # Handle building tags
-    f.add_transition (ANY, 'TAG', StartBuildTag, 'BUILD_TAG')
-    f.add_transition (ANY, 'BUILD_TAG', BuildTag, 'BUILD_TAG')
-    f.add_transition (' ', 'BUILD_TAG', None, 'ELEMENT_PARAMETERS')
-    f.add_transition ('/', 'TAG', None, 'END_TAG')
-    f.add_transition ('/', 'BUILD_TAG', None, 'EMPTY_ELEMENT')
+    f.add_transition_any ('TAG', StartBuildTag, 'BUILD_TAG')
+    f.add_transition_any ('BUILD_TAG', BuildTag, 'BUILD_TAG')
+    f.add_transition_list (string.whitespace, 'BUILD_TAG', Passthrough, 'ELEMENT_PARAMETERS')
+    f.add_transition ('/', 'TAG', Passthrough, 'END_TAG')
+    f.add_transition ('/', 'BUILD_TAG', Passthrough, 'EMPTY_ELEMENT')
     f.add_transition ('>', 'BUILD_TAG', DoneBuildTag, 'INIT')
 
     # Handle element parameters
     f.add_transition ('>', 'ELEMENT_PARAMETERS', DoneBuildTag, 'INIT')
-    f.add_transition ('/', 'ELEMENT_PARAMETERS', None, 'EMPTY_ELEMENT')
-    f.add_transition ('"', 'ELEMENT_PARAMETERS', None, 'DOUBLE_QUOTE')
-    f.add_transition (ANY, 'ELEMENT_PARAMETERS', None, 'ELEMENT_PARAMETERS')
+    f.add_transition ('/', 'ELEMENT_PARAMETERS', Passthrough, 'EMPTY_ELEMENT')
+    f.add_transition ('"', 'ELEMENT_PARAMETERS', Passthrough, 'DOUBLE_QUOTE')
+    f.add_transition_any ('ELEMENT_PARAMETERS', Passthrough, 'ELEMENT_PARAMETERS')
 
     # Handle quoting inside of parameter lists
-    f.add_transition (ANY, 'DOUBLE_QUOTE', None, 'DOUBLE_QUOTE')
-    f.add_transition ('"', 'DOUBLE_QUOTE', None, 'ELEMENT_PARAMETERS')
+    f.add_transition_any ('DOUBLE_QUOTE', Passthrough, 'DOUBLE_QUOTE')
+    f.add_transition ('"', 'DOUBLE_QUOTE', Passthrough, 'ELEMENT_PARAMETERS')
 
     # Handle empty element tags
     f.add_transition ('>', 'EMPTY_ELEMENT', DoneEmptyElement, 'INIT')
 
     # Handle end tags
-    f.add_transition (ANY, 'END_TAG', StartBuildEndTag, 'BUILD_END_TAG')
-    f.add_transition (ANY, 'BUILD_END_TAG', BuildEndTag, 'BUILD_END_TAG')
+    f.add_transition_any ('END_TAG', StartBuildEndTag, 'BUILD_END_TAG')
+    f.add_transition_any ('BUILD_END_TAG', BuildEndTag, 'BUILD_END_TAG')
     f.add_transition ('>', 'BUILD_END_TAG', DoneBuildEndTag, 'INIT')
 
     f.process_string (XML_TEST_DATA)
     
+    print
     if len(f.stack) == 0:
         print 'XML file is valid.'
     else:
@@ -230,4 +288,10 @@ def test():
         print f.stack
 
 if __name__ == '__main__':
-    test ()
+    '''Finite State Machine and push-down Automata (FSM and PDA)
+    
+    Here is a simple little Finite State Machine. It includes a stack. 
+    This is useful for little parsing tasks. Included is a test which
+    reformats XML code.
+    '''
+    demo ()

0faccd2f58c0453d3fe7864b42438815e94f9ea2
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2002-08-27 21:16:08 +0000

    These tests did not work with the latest pexpect interface. I fixed this.

--- pexpect/tests/test_command_list_split.py
+++ pexpect/tests/test_command_list_split.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python
 import pexpect
 import unittest
 
 class SplitCommandLineTestCase(unittest.TestCase):
     #def runTest (self):
     def testSplitSizes(self):
-        assert len(pexpect._split_command_line(r'')) == 0
-        assert len(pexpect._split_command_line(r'one')) == 1
-        assert len(pexpect._split_command_line(r'one two')) == 2
-        assert len(pexpect._split_command_line(r'one\ one')) == 1
-        assert len(pexpect._split_command_line('\'one one\'')) == 1
-        assert len(pexpect._split_command_line(r'one\"one')) == 1
-        assert len(pexpect._split_command_line(r'This\' is a\'\ test')) == 3
+        assert len(pexpect.split_command_line(r'')) == 0
+        assert len(pexpect.split_command_line(r'one')) == 1
+        assert len(pexpect.split_command_line(r'one two')) == 2
+        assert len(pexpect.split_command_line(r'one\ one')) == 1
+        assert len(pexpect.split_command_line('\'one one\'')) == 1
+        assert len(pexpect.split_command_line(r'one\"one')) == 1
+        assert len(pexpect.split_command_line(r'This\' is a\'\ test')) == 3
 
 if __name__ == '__main__':
     unittest.main()

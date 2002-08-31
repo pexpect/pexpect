615a406cd979dbfec8ba2209036bf8bea1ba035e
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2002-08-31 13:29:02 +0000

    Added test to expect as well as expect_exact. Not a GREAT test, but it checks that it basically works against the standard Python commands module.

--- pexpect/tests/test_expect.py
+++ pexpect/tests/test_expect.py
@@ -5,6 +5,7 @@ import commands
 
 class ExpectTestCase(unittest.TestCase):
     #def runTest (self):
+	
     def test_expectExact (self):
         the_old_way = commands.getoutput('/bin/ls -l')
 
@@ -13,21 +14,26 @@ class ExpectTestCase(unittest.TestCase):
 		the_new_way = ''
 		while 1:
 			p.expect_exact ('\n')
-			the_new_way = the_new_way + p.before# + '\n'
+			the_new_way = the_new_way + p.before
 	except:
 		the_new_way = the_new_way[:-1]
 		the_new_way = the_new_way.replace('\r','\n')
 
-	print type(the_old_way)
-	print the_old_way
-	print the_new_way
+	assert the_old_way == the_new_way
 
-	fout = open('kill1','wb')
-	fout.write(the_old_way)
-	fout.close
-	fout = open('kill2', 'wb')
-	fout.write(the_new_way)
-	fout.close
+    def test_expect (self):
+        the_old_way = commands.getoutput('/bin/ls -l')
+
+	p = pexpect.spawn('/bin/ls -l')
+	try:
+		the_new_way = ''
+		while 1:
+			p.expect ('\n')
+			the_new_way = the_new_way + p.before
+	except:
+		the_new_way = the_new_way[:-1]
+		the_new_way = the_new_way.replace('\r','\n')
+
 	assert the_old_way == the_new_way
 
 

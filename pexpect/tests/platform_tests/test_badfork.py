f1a6c07f6e601f22ab89a9b83dec082e909ee869
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2002-08-31 13:55:33 +0000

    Moved non pyunittest test files to platform_tests/ directory.

--- pexpect/tests/platform_tests/test_badfork.py
+++ pexpect/tests/platform_tests/test_badfork.py
@@ -7,7 +7,7 @@ def main ():
 	print 'pid', pid
 	print 'fd', fd
 	Xexpect(fd, 'bash.*#',10)
-	os.write(fd, 'scp -P 6666 *.py noah@gw.tiered.com:expyct/\n')
+	os.write(fd, 'scp -P 6666 *.py noah@gw.tiered.com:pexpect/\n')
 	Xexpect(fd, 'bash.*#',10)
 	os.write(fd, 'exit\n')
 	print _my_read (fd, 1000, 5)

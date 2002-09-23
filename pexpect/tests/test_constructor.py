8d7f910b88fb3bb3f5f86a5f2bdbd7eada176544
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2002-09-23 06:53:09 +0000

    Test based on / is not a good idea. I made the test look at /bin.

--- pexpect/tests/test_constructor.py
+++ pexpect/tests/test_constructor.py
@@ -10,11 +10,10 @@ class TestCaseConstructor(unittest.TestCase):
 	the same results for different styles of invoking __init__().
 	This assumes that the root directory / is static during the test.
 	"""
-        p1 = pexpect.spawn('/bin/ls -l /')
-        p2 = pexpect.spawn('/bin/ls' ,['-l', '/'])
+        p1 = pexpect.spawn('/bin/ls -l /bin')
+        p2 = pexpect.spawn('/bin/ls' ,['-l', '/bin'])
 	p1.expect (pexpect.EOF)
 	p2.expect (pexpect.EOF)
-
         assert (p1.before == p2.before)
 
     def test_named_parameters (self):

db79e2d12cae00420b856b95f825fa426ae4640f
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2002-10-16 19:14:06 +0000

    Adding support for systems that are up less than 1 hour.

--- pexpect/examples/uptime.py
+++ pexpect/examples/uptime.py
@@ -11,6 +11,8 @@ import re
 #
 # [x86] Linux 2.4 (Redhat 7.3)
 #  2:06pm  up 63 days, 18 min,  3 users,  load average: 0.32, 0.08, 0.02
+# [x86] Linux 2.4.18-14 (Redhat 8.0)
+#  3:07pm  up 29 min,  1 user,  load average: 2.44, 2.51, 1.57
 # [PPC - G4] MacOS X 10.1 SERVER Edition
 # 2:11PM  up 3 days, 13:50, 3 users, load averages: 0.01, 0.00, 0.00
 # [Sparc - R220] Sun Solaris (8)
@@ -19,7 +21,9 @@ import re
 p = pexpect.spawn ('uptime')
 p.expect ('up ([0-9]+) days,.*?,\s+([0-9]+) users,\s+load average[s]*: ([0-9]+\.[0-9][0-9]), ([0-9]+\.[0-9][0-9]), ([0-9]+\.[0-9][0-9])')
 
-days, users, av1, av5, av15 = p.match.groups()
+duration, users, av1, av5, av15 = p.match.groups()
 
-print '%s days, %s users, %s (1 min), %s (5 min), %s (15 min)' % (
-    days, users, av1, av5, av15)
+print '%s time, %s users, %s (1 min), %s (5 min), %s (15 min)' % (
+    duration, users, av1, av5, av15)
+
+

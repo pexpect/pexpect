2d86397998887f580a451f202e2101d706ff4248
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2003-02-04 20:39:37 +0000

    Eat my own dog food. Why do this with a shell script when it's easier to do with pexpect? So I got rid of the shell script.

---
+++ pexpect/tools/websync.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# I use this to keep the sourceforge pages up to date with the
+# latest documentation and I like to keep a copy of the distribution
+# on the web site so that it will be compatible with
+# The Vaults of Parnasus which requires a direct URL link to a
+# tar ball distribution. I don't advertise the package this way.
+
+import pexpect
+import getpass
+
+X = getpass.getpass('Password: ')
+
+p = pexpect.spawn ('ssh noah@use-pr-shell1.sourceforge.net "cd htdocs;rm index.html;wget http://www.noah.org/python/pexpect/index.html"')
+p.expect ('password:')
+p.sendline (X)
+p.expect (pexpect.EOF)
+print p.before
+
+p = pexpect.spawn ('ssh noah@use-pr-shell1.sourceforge.net "cd htdocs;rm clean.css;wget http://www.noah.org/python/pexpect/clean.css"')
+p.expect ('password:')
+p.sendline (X)
+p.expect (pexpect.EOF)
+print p.before
+
+p = pexpect.spawn ('scp pexpect-doc.tgz noah@use-pr-shell1.sourceforge.net:htdocs/pexpect-doc.tgz')
+p.expect ('password:')
+p.sendline (X)
+p.expect (pexpect.EOF)
+print p.before
+
+p = pexpect.spawn ('ssh noah@use-pr-shell1.sourceforge.net "cd htdocs;tar zxvf pexpect-doc.tgz"')
+p.expect ('password:')
+p.sendline (X)
+p.expect (pexpect.EOF)
+print p.before
+
+p = pexpect.spawn ('scp pexpect-current.tgz noah@use-pr-shell1.sourceforge.net:htdocs/pexpect-current.tgz')
+p.expect ('password:')
+p.sendline (X)
+p.expect (pexpect.EOF)
+print p.before
+

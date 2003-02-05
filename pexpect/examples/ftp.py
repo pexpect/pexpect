86ab7259712bdb41d59c8edd490c74db8dca8a58
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2003-02-05 17:39:07 +0000

    Added some comments and more status printing.

--- pexpect/examples/ftp.py
+++ pexpect/examples/ftp.py
@@ -12,7 +12,7 @@ child = pexpect.spawn('/usr/bin/ftp ftp.openbsd.org')
 child.expect('Name .*: ')
 child.sendline('anonymous')
 child.expect('Password:')
-child.sendline('pexpect@sf.net')
+child.sendline('pexpect@sourceforge.net')
 child.expect('ftp> ')
 child.sendline('cd /pub/OpenBSD/3.1/packages/i386')
 child.expect('ftp> ')
@@ -26,9 +26,25 @@ print("Escape character is '^]'.\n")
 sys.stdout.write (child.after)
 sys.stdout.flush()
 child.interact() # Escape character defaults to ^]
+# At this point this script blocks until the user presses the escape character
+# or until the child exits. The human user and the child should be talking
+# to each other now.
 
+# At this point the script is running again.
+print 'Left interact mode.'
+
+#
+# The rest is not strictly necessary. This just demonstrates a few functions.
+# This makes sure the child is dead; although it would be killed when Python exits.
+#
 if child.isalive():
-    child.sendline('bye')
-    child.kill(1)
-print 'Is Alive: ', child.isalive()
+    child.sendline('bye') # Try to ask ftp child to exit.
+    child.kill(1) # Then try to force it.
+# Print the final state of the child. Normally isalive() should be FALSE.
+print
+if child.isalive():
+    print 'Child did not exit gracefully.'
+else:
+    print 'Child exited gracefully.'
+
 

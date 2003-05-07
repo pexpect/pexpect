9c1a00f6b4dd493d0d0b57fcf94fbfa0a5a237e5
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2003-05-07 23:59:53 +0000

    Reports the terminal window size on SIGWINCH signal.

---
+++ pexpect/tests/sigwinch_report.py
@@ -0,0 +1,16 @@
+import time, struct, fcntl, termios, os, sys
+import signal, os
+
+def getwinsize():
+    s = struct.pack('HHHH', 0, 0, 0, 0)
+    x = fcntl.ioctl(sys.stdout.fileno(), termios.TIOCGWINSZ, s)
+    return struct.unpack('HHHH', x)
+
+def handler(signum, frame):
+    print 'SIGWINCH:', getwinsize ()
+
+signal.signal(signal.SIGWINCH, handler)
+
+while 1:
+	time.sleep(10)
+

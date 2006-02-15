2ad6bcb23292d1beb142e55e481bef754f5792b5
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2006-02-15 22:34:57 +0000

    Making improvements, slow but sure.

--- pexpect/examples/ssh_tunnel.py
+++ pexpect/examples/ssh_tunnel.py
@@ -2,34 +2,28 @@
 """This starts an SSH tunnel to a given host.
 If the SSH process ever dies then this script will detect that and restart it.
 I use this under Cygwin to keep open encrypted tunnels to
-port 110 (POP3) and port 25 (SMTP). I set my mail client to talk to
+port 25 (SMTP), port 143 (IMAP4), and port 110 (POP3). I set my mail client to talk to
 localhost and I keep this script running in the background.
 
 Note that this is a rather stupid script at the moment because it
 just looks to see if any ssh process is running. It should really
 make sure that our specific ssh process is running.
-
 ssh is missing a very useful feature. It has no way to report the
 process id of the background daemon that it creates with the -f command.
-Most annoying!
-
+This would be a really useful script if I could figure a way around this problem.
 """
 import pexpect
 import getpass
 import time
 
-#tunnel_command = 'ssh -C -N  -L 25:%(host)s:25 -L 110:%(host)s:110 %(user)s@%(host)s'
-tunnel_command = 'ssh -C -n -L 25:%(host)s:25 -L 110:%(host)s:110 %(user)s@%(host)s -f nothing.sh'
-nothing_script = """#!/bin/sh
-while true; do sleep 53; done
-"""
+# SMTP:25 IMAP4:143 POP3:110
+tunnel_command = 'ssh -C -N -f -L 25:127.0.0.1:25 -L 143:127.0.0.1:143 -L 110:127.0.0.1:110 %(user)@%(host)'
 host = raw_input('Hostname: ')
 user = raw_input('Username: ')
 X = getpass.getpass('Password: ')
 
 def get_process_info ():
     # This seems to work on both Linux and BSD, but should otherwise be considered highly UNportable.
-    # Basic process info is one of those glaring features missing from POSIX.
     ps = pexpect.run ('ps ax -O ppid')
     pass
 def start_tunnel ():
@@ -65,4 +59,10 @@ def main ():
 
 if __name__ == '__main__':
     main ()
+
+# This was for older SSH versions that didn't have -f option
+#tunnel_command = 'ssh -C -n -L 25:%(host)s:25 -L 110:%(host)s:110 %(user)s@%(host)s -f nothing.sh'
+#nothing_script = """#!/bin/sh
+#while true; do sleep 53; done
+#"""
 

ccf990f948f43f5688a35a2cea9fd8ebc1e2706f
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2006-05-30 20:35:45 +0000

    better

--- pexpect/tools/tweak_files.py
+++ pexpect/tools/tweak_files.py
@@ -2,9 +2,15 @@ import pyed
 import re
 
 # extract the version number from the pexpect.py source.
-doc = file ('../pexpect.py','rb').read()
-d = pyed.pyed(doc)
+d = pyed.pyed()
+d.read ("../pexpect.py")
 d.first('^__version__')
 r = re.search("'([0-9]\.[0-9])'", d.cur_line)
 version = r.group(1)
 
+d = pyed.pyed()
+d.read ("../doc/index.html")
+for cl in d.match_lines('.*VERSION.*'):
+    d.cur_line = d.cur_line.replace('VERSION', version)
+d.write("../doc/index.html") 
+    

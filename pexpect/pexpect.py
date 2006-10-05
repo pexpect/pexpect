4500e675efb38d835a817a56662811074fff08c1
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2006-10-05 20:24:12 +0000

    Added some documentation about why | and > don't work (because they aren't supposed to. | and > is done by a shell).

--- pexpect/pexpect.py
+++ pexpect/pexpect.py
@@ -237,16 +237,31 @@ class spawn (object):
     def __init__(self, command, args=[], timeout=30, maxread=2000, searchwindowsize=None, logfile=None, env=None):
         """This is the constructor. The command parameter may be a string
         that includes a command and any arguments to the command. For example:
-            p = pexpect.spawn ('/usr/bin/ftp')
-            p = pexpect.spawn ('/usr/bin/ssh user@example.com')
-            p = pexpect.spawn ('ls -latr /tmp')
+            child = pexpect.spawn ('/usr/bin/ftp')
+            child = pexpect.spawn ('/usr/bin/ssh user@example.com')
+            child = pexpect.spawn ('ls -latr /tmp')
         You may also construct it with a list of arguments like so:
-            p = pexpect.spawn ('/usr/bin/ftp', [])
-            p = pexpect.spawn ('/usr/bin/ssh', ['user@example.com'])
-            p = pexpect.spawn ('ls', ['-latr', '/tmp'])
+            child = pexpect.spawn ('/usr/bin/ftp', [])
+            child = pexpect.spawn ('/usr/bin/ssh', ['user@example.com'])
+            child = pexpect.spawn ('ls', ['-latr', '/tmp'])
         After this the child application will be created and
         will be ready to talk to. For normal use, see expect() and 
         send() and sendline().
+
+        Remember that Pexpect does NOT interpret shell meta characters
+        such as redirect, pipe, or wild cards (>, |, or *).
+        This is a common mistake. If you want to run a command and pipe
+        it through another command then you must also start a shell.
+        For example:
+            child = pexpect.spawn('/bin/bash -c "ls -l | grep LOG > log_list.txt"')
+            child.expect(pexpect.EOF)
+        The second form of spawn (where you pass a list of arguments) is
+        useful in situations where you wish to spawn a command and
+        pass it its own argument list. This can make syntax more clear.
+        For example, the following is equivalent to the previous example:
+            shell_cmd = 'ls -l | grep LOG > log_list.txt'
+            child = pexpect.spawn('/bin/bash', ['-c', shell_cmd])
+            child.expect(pexpect.EOF)
 
         The maxread attribute sets the read buffer size.
         This is maximum number of bytes that Pexpect will try to read

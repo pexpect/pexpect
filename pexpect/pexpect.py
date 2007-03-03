1cbd5a891a5dc8d879361033fb717b57fad780d9
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2007-03-03 01:58:17 +0000

    Added Andrew

--- pexpect/pexpect.py
+++ pexpect/pexpect.py
@@ -34,7 +34,7 @@ Credits: Noah Spurrier, Richard Holden, Marco Molteni, Kimberley Burchett,
 Robert Stone, Hartmut Goebel, Chad Schroeder, Erick Tryzelaar, Dave Kirby, Ids
 vander Molen, George Todd, Noel Taylor, Nicolas D. Cesar, Alexander Gattin,
 Geoffrey Marshall, Francisco Lourenco, Glen Mabey, Karthik Gurusamy, Fernando
-Perez, Corey Minyard, Jon Cohen, Guillaume Chazarain (Let me know if I forgot
+Perez, Corey Minyard, Jon Cohen, Guillaume Chazarain, Andrew Ryan (Let me know if I forgot
 anyone.)
 
 Free, open source, and all that good stuff.

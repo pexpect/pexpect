dd0221696463981f56a8bea0a2c8b4d5a5a342bc
noah <noah@656d521f-e311-0410-88e0-e7920216d269>
2008-05-23 20:46:01 +0000

    It turns out someone was getting another exception besides AttributeError.

--- pexpect/pexpect.py
+++ pexpect/pexpect.py
@@ -33,10 +33,10 @@ the normal stdio streams.
 Credits: Noah Spurrier, Richard Holden, Marco Molteni, Kimberley Burchett,
 Robert Stone, Hartmut Goebel, Chad Schroeder, Erick Tryzelaar, Dave Kirby, Ids
 vander Molen, George Todd, Noel Taylor, Nicolas D. Cesar, Alexander Gattin,
-Geoffrey Marshall, Francisco Lourenco, Glen Mabey, Karthik Gurusamy, Fernando
-Perez, Corey Minyard, Jon Cohen, Guillaume Chazarain, Andrew Ryan, Nick
-Craig-Wood, Andrew Stone, Jorgen Grahn, John Spiegel, Jan Grant (Let me know if
-I forgot anyone.)
+Jacques-Etienne Baudoux, Geoffrey Marshall, Francisco Lourenco, Glen Mabey,
+Karthik Gurusamy, Fernando Perez, Corey Minyard, Jon Cohen, Guillaume
+Chazarain, Andrew Ryan, Nick Craig-Wood, Andrew Stone, Jorgen Grahn, John
+Spiegel, Jan Grant (Let me know if I forgot anyone.)
 
 Free, open source, and all that good stuff.
 
@@ -444,7 +444,7 @@ class spawn (object):
             # -- Fernando Perez
             try:
                 self.close()
-            except AttributeError:
+            except:
                 pass
 
     def __str__(self):

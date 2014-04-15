afebd40190b443a818d713aa17c7a28a8d0b97cc
Thomas Kluyver <takowl@gmail.com>
2014-04-14 18:34:32 -0700

    Add long description (copied part of README)

--- setup.py
+++ setup.py
@@ -2,11 +2,28 @@ from distutils.core import setup
 
 from pexpect import __version__
 
+long_description = """
+Pexpect is a pure Python module for spawning child applications; controlling
+them; and responding to expected patterns in their output. Pexpect works like
+Don Libes' Expect. Pexpect allows your script to spawn a child application and
+control it as if a human were typing commands.
+
+Pexpect can be used for automating interactive applications such as ssh, ftp,
+passwd, telnet, etc. It can be used to a automate setup scripts for duplicating
+software package installations on different servers. It can be used for
+automated software testing. Pexpect is in the spirit of Don Libes' Expect, but
+Pexpect is pure Python. Unlike other Expect-like modules for Python, Pexpect
+does not require TCL or Expect nor does it require C extensions to be compiled.
+It should work on any platform that supports the standard Python pty module.
+The Pexpect interface was designed to be easy to use.
+"""
+
 setup (name='pexpect',
     version=__version__,
     py_modules=['pxssh', 'fdpexpect', 'FSM', 'screen', 'ANSI'],
     packages=['pexpect'],
     description='Pexpect allows easy control of interactive console applications.',
+    long_description=long_description,
     author='Noah Spurrier; Thomas Kluyver; Jeff Quast',
     author_email='noah@noah.org; thomas@kluyver.me.uk; contact@jeffquast.com',
     url='http://pexpect.readthedocs.org/',

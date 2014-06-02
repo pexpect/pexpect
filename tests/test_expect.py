ffe4bdad4872fdce56299681cbb0ff579f5ced71
Thomas Kluyver <takowl@gmail.com>
2014-06-01 20:29:24 -0700

    Ignore coverage env var if not set

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -76,7 +76,7 @@ class assert_raises_msg(object):
 def no_coverage_env():
     "Return a copy of os.environ that won't trigger coverage measurement."
     env = os.environ.copy()
-    env.pop('COV_CORE_SOURCE')
+    env.pop('COV_CORE_SOURCE', None)
     return env
 
 class ExpectTestCase (PexpectTestCase.PexpectTestCase):

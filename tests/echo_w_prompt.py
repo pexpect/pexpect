b0332f5e942450f464c6a661f2cdb7932b88e59b
Jeff Quast <contact@jeffquast.com>
2014-11-27 14:50:41 -0800

    revert echo/interact sub-proc UTF8 wrappers

--- tests/echo_w_prompt.py
+++ tests/echo_w_prompt.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import codecs
-import sys
-
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-sys.stderr = codecs.getwriter('utf8')(sys.stderr)
-
 
 try:
     raw_input

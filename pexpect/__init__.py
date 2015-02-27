e067741654f4d6c5498bdb11a2d703640dc32758
Thomas Kluyver <takowl@gmail.com>
2015-02-27 09:22:34 -0800

    Fix deprecated runu function

--- pexpect/__init__.py
+++ pexpect/__init__.py
@@ -220,8 +220,9 @@ def runu(command, timeout=30, withexitstatus=False, events=None,
         extra_args=None, logfile=None, cwd=None, env=None, **kwargs):
     """Deprecated: pass encoding to run() instead.
     """
+    kwargs.setdefault('encoding', 'utf-8')
     return run(command, timeout=timeout, withexitstatus=withexitstatus,
                 events=events, extra_args=extra_args, logfile=logfile, cwd=cwd,
-                env=env, _spawn=spawnu, **kwargs)
+                env=env, **kwargs)
 
 # vim: set shiftround expandtab tabstop=4 shiftwidth=4 ft=python autoindent :

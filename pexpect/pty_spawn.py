803cdc108e3942bc541ce5e0558604dbf8455142
Kristopher H <iix.ftw@gmail.com>
2016-10-19 23:31:11 +0200

    Raise ExceptionPexpect instead of PtyProcessError in spawn.close

--- pexpect/pty_spawn.py
+++ pexpect/pty_spawn.py
@@ -315,7 +315,10 @@ class spawn(SpawnBase):
         and SIGINT). '''
 
         self.flush()
-        self.ptyproc.close(force=force)
+        with _wrap_ptyprocess_err():
+            # PtyProcessError may be raised if it is not possible to terminate
+            # the child.
+            self.ptyproc.close(force=force)
         self.isalive()  # Update exit status from ptyproc
         self.child_fd = -1
 

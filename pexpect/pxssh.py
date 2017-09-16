0e934f63967c1e95244a06659cc307a773b7729e
john <johndowling01@live.co.uk>
2017-09-16 16:50:24 +0100

    cleanup and add docstrings back into the code

--- pexpect/pxssh.py
+++ pexpect/pxssh.py
@@ -1,4 +1,5 @@
-'''This class extends pexpect.spawn to specialize setting up SSH connections.
+"""
+This class extends pexpect.spawn to specialize setting up SSH connections.
 This adds methods for login, logout, and expecting the shell prompt.
 
 PEXPECT LICENSE
@@ -17,12 +18,10 @@ PEXPECT LICENSE
     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
 
-'''
 
 from pexpect import ExceptionPexpect, TIMEOUT, EOF, spawn
-from pprint import pprint
-import copy
 
 
 __all__ = ['ExceptionPxssh', 'pxssh']
@@ -33,6 +32,61 @@ class ExceptionPxssh(ExceptionPexpect):
 
 
 class pxssh(spawn):
+
+    """
+        This class extends pexpect.spawn to specialize setting up SSH
+        connections. This adds methods for login, logout, and expecting the shell
+        prompt. It does various tricky things to handle many situations in the SSH
+        login process. For example, if the session is your first login, then pxssh
+        automatically accepts the remote certificate; or if you have public key
+        authentication setup then pxssh won't wait for the password prompt.
+        pxssh uses the shell prompt to synchronize output from the remote host. In
+        order to make this more robust it sets the shell prompt to something more
+        unique than just $ or #. This should work on most Borne/Bash or Csh style
+        shells.
+        Example that runs a few commands on a remote server and prints the result::
+            from pexpect import pxssh
+            import getpass
+            try:
+                s = pxssh.pxssh()
+                hostname = raw_input('hostname: ')
+                username = raw_input('username: ')
+                password = getpass.getpass('password: ')
+                s.login(hostname, username, password)
+                s.sendline('uptime')   # run a command
+                s.prompt()             # match the prompt
+                print(s.before)        # print everything before the prompt.
+                s.sendline('ls -l')
+                s.prompt()
+                print(s.before)
+                s.sendline('df')
+                s.prompt()
+                print(s.before)
+                s.logout()
+            except pxssh.ExceptionPxssh as e:
+                print("pxssh failed on login.")
+                print(e)
+        Example showing how to specify SSH options::
+            from pexpect import pxssh
+            s = pxssh.pxssh(options={
+                                "StrictHostKeyChecking": "no",
+                                "UserKnownHostsFile": "/dev/null"})
+            ...
+        Note that if you have ssh-agent running while doing development with pxssh
+        then this can lead to a lot of confusion. Many X display managers (xdm,
+        gdm, kdm, etc.) will automatically start a GUI agent. You may see a GUI
+        dialog box popup asking for a password during development. You should turn
+        off any key agents during testing. The 'force_password' attribute will turn
+        off public key authentication. This will only work if the remote SSH server
+        is configured to allow password logins. Example of using 'force_password'
+        attribute::
+                s = pxssh.pxssh()
+                s.force_password = True
+                hostname = raw_input('hostname: ')
+                username = raw_input('username: ')
+                password = getpass.getpass('password: ')
+                s.login (hostname, username, password)
+    """
 
     _PROMPT_ESCAPES =["$", "(", ")", "[", "]"]
     _REGEX_STRINGS = {
@@ -244,6 +298,20 @@ class pxssh(spawn):
                 self.PROMPT = self.PROMPT.replace(escape, "\{0}".format(escape))
 
     def prompt(self, timeout=-1):
+
+        """
+            Match the next shell prompt.
+            This is little more than a short-cut to the :meth:`~pexpect.spawn.expect`
+            method. Note that if you called :meth:`login` with
+            ``auto_prompt_reset=False``, then before calling :meth:`prompt` you must
+            set the :attr:`PROMPT` attribute to a regex that it will use for
+            matching the prompt.
+            Calling :meth:`prompt` will erase the contents of the :attr:`before`
+            attribute even if no prompt is ever matched. If timeout is not given or
+            it is set to -1 then self.timeout is used.
+            :return: True if the shell prompt was matched, False if the timeout was
+                 reached.
+        """
 
         if timeout == -1:
             timeout = self.timeout

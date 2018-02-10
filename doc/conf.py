8944656b1c1b47f76cdb3d209fa1bf27ffb0c979
Thomas Kluyver <thomas@kluyver.me.uk>
2018-02-10 12:34:56 +0000

    Version number -> 4.4.0

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ copyright = u'2013, Noah Spurrier and contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '4.3.1'
+version = '4.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 

5daa766b6d0d0efd7e4e51c72a0b5181e58d2e4c
dluyer <53582923+dluyer@users.noreply.github.com>
2020-01-10 11:55:59 -0800

    Whitespace cleanup.

--- pexpect/run.py
+++ pexpect/run.py
@@ -49,7 +49,7 @@ def run(command, timeout=30, withexitstatus=False, events=None,
         (command_output, exitstatus) = run('ls -l /bin', withexitstatus=1)
 
     The following will run SSH and execute 'ls -l' on the remote machine. The
-    password 'secret' will be sent if the '(?i)password' pattern is ever seen::
+    password 'secret' will be sent if the '(?i)password' pattern is ever seen:
 
         run("ssh username@machine.example.com 'ls -l'",
             events={'(?i)password':'secret\\n'})
@@ -67,7 +67,7 @@ def run(command, timeout=30, withexitstatus=False, events=None,
     contains patterns and responses. Whenever one of the patterns is seen
     in the command output, run() will send the associated response string.
     So, run() in the above example can be also written as:
-    
+
         run("mencoder dvd://1 -o video.avi -oac copy -ovc copy",
             events=[(TIMEOUT,print_ticks)], timeout=5)
 

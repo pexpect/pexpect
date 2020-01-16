47bcd7bd56f3c7c8c409604ad25d462b95ea58f3
dluyer <53582923+dluyer@users.noreply.github.com>
2020-01-16 14:06:49 -0800

    Update version # for new release

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ copyright = u'2013, Noah Spurrier and contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '4.7'
+version = '4.8'
 # The full version, including alpha/beta/rc tags.
 release = version
 

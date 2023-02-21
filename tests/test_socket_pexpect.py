ce99a6b49e150ccd72dc1696dab923b586f42c4c
Tapple <mfulmer@cisco.com>
2023-02-21 14:24:58 -0500

    renamed module socket_expect to socket_pexpect

--- tests/test_socket_pexpect.py
+++ tests/test_socket_pexpect.py
@@ -19,7 +19,7 @@ PEXPECT LICENSE
 
 '''
 import pexpect
-from pexpect import socket_expect
+from pexpect import socket_pexpect
 import unittest
 from . import PexpectTestCase
 import socket
@@ -38,14 +38,14 @@ class ExpectTestCase(PexpectTestCase.PexpectTestCase):
 
     def test_socket (self):
         socket = open_file_socket('TESTDATA.txt')
-        s = socket_expect.SocketSpawn(socket)
+        s = socket_pexpect.SocketSpawn(socket)
         s.expect(b'This is the end of test data:')
         s.expect(pexpect.EOF)
         self.assertEqual(s.before, b' END\n')
 
     def test_maxread (self):
         socket = open_file_socket('TESTDATA.txt')
-        s = socket_expect.SocketSpawn(socket)
+        s = socket_pexpect.SocketSpawn(socket)
         s.maxread = 100
         s.expect('2')
         s.expect ('This is the end of test data:')
@@ -54,14 +54,14 @@ class ExpectTestCase(PexpectTestCase.PexpectTestCase):
 
     def test_socket_isalive (self):
         socket = open_file_socket('TESTDATA.txt')
-        s = socket_expect.SocketSpawn(socket)
+        s = socket_pexpect.SocketSpawn(socket)
         assert s.isalive()
         s.close()
         assert not s.isalive(), "Should not be alive after close()"
 
     def test_socket_isatty (self):
         socket = open_file_socket('TESTDATA.txt')
-        s = socket_expect.SocketSpawn(socket)
+        s = socket_pexpect.SocketSpawn(socket)
         assert not s.isatty()
         s.close()
 

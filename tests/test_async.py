0d95f9029da0656ac5e865af1135256f33414149
Andrew Shirayev <andrews239@gmail.com>
2024-02-02 16:31:19 +0300

    new tests in test_async.py: terminate/waitnoecho/readline/readlines/write/writelines

--- tests/test_async.py
+++ tests/test_async.py
@@ -12,6 +12,7 @@ from pexpect import replwrap
 
 from . import PexpectTestCase
 
+_CAT_EOF = b'^D\x08\x08'
 
 @unittest.skipIf(asyncio is None, "Requires asyncio")
 class AsyncTests(PexpectTestCase.AsyncPexpectTestCase):
@@ -85,3 +86,84 @@ class AsyncTests(PexpectTestCase.AsyncPexpectTestCase):
         # Check that the REPL was reset (SIGINT) after the incomplete input
         res = await bash.run_command("echo '1 2\n3 4'", async_=True)
         self.assertEqual(res.strip().splitlines(), ["1 2", "3 4"])
+
+    async def test_terminate_async(self):
+        " test force terminate always succeeds (SIGKILL) in async mode. "
+        child = pexpect.spawn('cat')
+        await child.terminate(force=1, async_=True)
+        await child.terminate(force=1, async_=True)
+        assert child.terminated
+
+    async def test_waitnoecho_async(self):
+        " Tests setecho(False) followed by waitnoecho() "
+        p = pexpect.spawn('cat', echo=False, timeout=5)
+        try:
+            p.setecho(False)
+            await p.waitnoecho(async_=True)
+        except IOError:
+            if sys.platform.lower().startswith('sunos'):
+                if hasattr(unittest, 'SkipTest'):
+                    raise unittest.SkipTest("Not supported on this platform.")
+                return 'skip'
+            raise
+
+    async def test_readline_async(self):
+        " Test spawn.readline(). "
+        # when argument 0 is sent, nothing is returned.
+        # Otherwise the argument value is meaningless.
+        child = pexpect.spawn('cat', echo=False)
+        await child.sendline("alpha", async_=True)
+        await child.sendline("beta", async_=True)
+        await child.sendline("gamma", async_=True)
+        await child.sendline("delta", async_=True)
+        child.sendeof()
+        assert await child.readline(0, async_=True) == b''
+        assert (await child.readline(async_=True)).rstrip() == b'alpha'
+        assert (await child.readline(1, async_=True)).rstrip() == b'beta'
+        assert (await child.readline(2, async_=True)).rstrip() == b'gamma'
+        assert (await child.readline(async_=True)).rstrip() == b'delta'
+        await child.expect(pexpect.EOF, async_=True)
+        assert not child.isalive()
+        assert child.exitstatus == 0
+
+#    async def test_iter_async(self):
+#        " iterating over lines of spawn.__iter__(). "
+#        child = pexpect.spawn('cat', echo=False)
+#        child.sendline("abc")
+#        child.sendline("123")
+#        child.sendeof()
+#        # Don't use ''.join() because we want to test __iter__().
+#        page = b''
+#        for line in child:
+#            page += line
+#        page = page.replace(_CAT_EOF, b'')
+#        assert page == b'abc\r\n123\r\n'
+
+    async def test_readlines_async(self):
+        " reading all lines of spawn.readlines(). "
+        child = pexpect.spawn('cat', echo=False)
+        await child.sendline("abc", async_=True)
+        await child.sendline("123", async_=True)
+        child.sendeof()
+        page = b''.join(await child.readlines(async_=True)).replace(_CAT_EOF, b'')
+        assert page == b'abc\r\n123\r\n'
+        await child.expect(pexpect.EOF, async_=True)
+        assert not child.isalive()
+        assert child.exitstatus == 0
+
+    async def test_write_async(self):
+        " write a character and return it in return. "
+        child = pexpect.spawn('cat', echo=False)
+        await child.write('a', async_=True)
+        await child.write('\r', async_=True)
+        self.assertEqual(await child.readline(async_=True), b'a\r\n')
+
+    async def test_writelines_async(self):
+        " spawn.writelines() "
+        child = pexpect.spawn('cat')
+        # notice that much like file.writelines, we do not delimit by newline
+        # -- it is equivalent to calling write(''.join([args,]))
+        await child.writelines(['abc', '123', 'xyz', '\r'], async_=True)
+        child.sendeof()
+        line = await child.readline(async_=True)
+        assert line == b'abc123xyz\r\n'
